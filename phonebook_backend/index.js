c72958e1295f9f513d3a4dfd7a2ab6edf92ca68e
TokoNew <tornikekhabeishvili07@gmail.com>
2023-07-30 12:29:02 +0400

    finished 3.5

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let phonebook = [
     { 
       "id": 1,
@@ -49,6 +51,23 @@ app.delete('/api/persons/:id', (request, response) => {
     phonebook = phonebook.filter(person => person.id !== id)
 
     response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'name or number missing' 
+    })
+  }
+  
+  const person = {
+    id: Math.floor(Math.random() * (158 - 15 + 1)) + 15,
+    name: body.name,
+    number: body.number
+  }
+  phonebook = phonebook.concat(person)
+  response.json(person)
 })
 
 const PORT = 3001
