b890d38be365409a537537366b716ed1c85611a6
TokoNew <tornikekhabeishvili07@gmail.com>
2023-07-31 16:38:13 +0400

    finished 3.6

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -60,6 +60,12 @@ app.post('/api/persons', (request, response) => {
       error: 'name or number missing' 
     })
   }
+
+  if (phonebook.some(person => person.name === body.name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
   
   const person = {
     id: Math.floor(Math.random() * (158 - 15 + 1)) + 15,
