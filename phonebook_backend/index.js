5c44135873b4c75404bd5727d3f5c077be4c2cd0
TokoNew <tornikekhabeishvili07@gmail.com>
2023-12-18 04:47:40 +0400

    completed 3.17

--- phonebook_backend/index.js
+++ phonebook_backend/index.js
@@ -27,7 +27,6 @@ app.get('/api/persons', (request, response) => {
       .then(people => {
           response.json(people);
       })
-      .catch(error => next(error))
 });
 
 app.get('/info', (request, response) => {
@@ -35,35 +34,52 @@ app.get('/info', (request, response) => {
     response.send(`<p>phonebook has data for ${Person.length} people</p> <p>${timestamp}</p>`)
 });
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     response.json(person)
-  })
+  }).catch(error => next(error))
 });
 
-app.post('/api/persons', (request, response) => {
+
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   if (!body.name || !body.number) {
     return response.status(400).json({ 
       error: 'name or number missing' 
     })
   }
+
   const person = new Person({
     name: body.name,
     number: body.number,
   })
   
   person.save().then(savedPerson => {
-    response.json(savedPerson)
+    response.json(savedPerson).catch(error => next(error))
   })
 })
   
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end()
       console.log(request.params.id)
-    })
+    }).catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+  .then(updatedPerson => {
+    response.json(updatedPerson)
+  })
+  .catch(error => next(error))
 })
 
 app.use(errorHandler)
